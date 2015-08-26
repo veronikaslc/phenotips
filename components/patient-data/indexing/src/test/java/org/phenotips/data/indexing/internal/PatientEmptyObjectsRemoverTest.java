e10c91c6c1fb3e074fce6e1aef47aafb7100332e
Tarang <tarang.marathe@gmail.com>
2015-08-26 15:25:38 -0400

    completed PatientEmptyObjectsRemover tests

--- components/patient-data/indexing/src/test/java/org/phenotips/data/indexing/internal/PatientEmptyObjectsRemoverTest.java
+++ components/patient-data/indexing/src/test/java/org/phenotips/data/indexing/internal/PatientEmptyObjectsRemoverTest.java
@@ -2,31 +2,35 @@ package org.phenotips.data.indexing.internal;
 
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.BaseObject;
+import com.xpn.xwiki.objects.BaseProperty;
+import com.xpn.xwiki.objects.BaseStringProperty;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.api.mockito.PowerMockito;
 import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.context.Execution;
 import org.xwiki.context.ExecutionContext;
 import org.xwiki.model.reference.EntityReference;
-import org.xwiki.observation.AbstractEventListener;
+import org.xwiki.observation.EventListener;
 import org.xwiki.observation.event.Event;
 import org.xwiki.test.mockito.MockitoComponentMockingRule;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.doReturn;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 public class PatientEmptyObjectsRemoverTest {
 
     @Rule
-    public MockitoComponentMockingRule<AbstractEventListener> mocker =
-            new MockitoComponentMockingRule<AbstractEventListener>(PatientEmptyObjectsRemover.class);
+    public MockitoComponentMockingRule<EventListener> mocker =
+        new MockitoComponentMockingRule<EventListener>(PatientEmptyObjectsRemover.class);
     @Mock
     private XWikiContext context;
 
@@ -40,28 +44,43 @@ public class PatientEmptyObjectsRemoverTest {
 
     private PatientEmptyObjectsRemover patientEmptyObjectsRemover;
 
+    private List<BaseObject> xWikiObjects;
+
+    @Mock
     private XWikiDocument xWikiDocument;
 
     @Before
     public void setUp() throws ComponentLookupException {
-        MockitoAnnotations.initMocks(this);
 
-        xWikiDocument = mock(XWikiDocument.class);
+        MockitoAnnotations.initMocks(this);
+        this.patientEmptyObjectsRemover = (PatientEmptyObjectsRemover) this.mocker.getComponentUnderTest();
 
         this.execution = this.mocker.getInstance(Execution.class);
+
         doReturn(this.executionContext).when(this.execution).getContext();
         doReturn(this.context).when(this.executionContext).getProperty("xwikicontext");
         doReturn(this.xWiki).when(this.context).getWiki();
-
-        this.patientEmptyObjectsRemover = (PatientEmptyObjectsRemover) this.mocker.getComponentUnderTest();
-
+        xWikiObjects = new ArrayList<BaseObject>();
+        doReturn(xWikiObjects).when(xWikiDocument).getXObjects(any(EntityReference.class));
     }
 
     @Test
-    public void emptyObjectRemovalTest() throws Exception {
-        whenNew(XWikiDocument.class).withArguments(Object.class).thenReturn(xWikiDocument);
-        doReturn(null).when(xWikiDocument).getXObject(any(EntityReference.class));
-        this.patientEmptyObjectsRemover.onEvent(mock(Event.class), null, mock(Object.class));
-        verify(this.xWiki).saveDocument(null, "Removed empty object", true, context);
+    public void emptyObjectRemovedTest() throws XWikiException {
+
+        BaseObject obj = mock(BaseObject.class);
+        xWikiObjects.add(obj);
+        BaseStringProperty property = mock(BaseStringProperty.class);
+        doReturn(property).when(obj).getField(anyString());
+        doReturn("").when(property).getValue();
+
+        this.patientEmptyObjectsRemover.onEvent(mock(Event.class), xWikiDocument, mock(Object.class));
+        verify(this.xWikiDocument, times(2)).removeXObject((BaseObject) anyObject());
+        verify(this.xWiki, times(2)).saveDocument(this.xWikiDocument, "Removed empty object", true, this.context);
+    }
+
+    @Test
+    public void exitOnNullObjectTest() {
+        this.patientEmptyObjectsRemover.onEvent(mock(Event.class), xWikiDocument, mock(Object.class));
+        verify(this.xWikiDocument, never()).removeXObject((BaseObject) anyObject());
     }
 }
